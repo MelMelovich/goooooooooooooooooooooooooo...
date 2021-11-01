8e6de85f7a41411b9312026d952c23709b6da5c6
Mele_fh60@student.itstep.org <Mele_fh60@student.itstep.org>
2021-11-01 17:55:49 +0200

    Добавьте файлы проекта.

---
+++ даааааааааааааваааааааааааааааааааааай/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace даааааааааааааваааааааааааааааааааааай
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("даааааааааааааааааааааааваааааааааааааааааааай");
+        }
+    }
+}
