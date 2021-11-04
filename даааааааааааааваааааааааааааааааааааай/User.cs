4d08b7d4abe6912a528a0ca278c562b9f1a51c41
totalcoder <48204819+totalcoder@users.noreply.github.com>
2021-11-04 23:52:26 +0200

    Create User.cs
    Добавлен класс пользователя

---
+++ даааааааааааааваааааааааааааааааааааай/User.cs
@@ -0,0 +1,27 @@
+using System;
+// Класс пользователя 
+// содержит код пользователя и Имя пользователя
+
+public class User
+{
+  private int id;
+  private string Name;
+  
+  // Конструктор Класса 
+  // передаем id и Name
+	public User(int id_, string Name_)
+	{
+	  id = id_;
+    Name = Name_;
+	}
+	
+  
+  private string GetName(){
+   return Name;
+  }  
+ 
+  private int GetId(){
+   return id;
+  }  
+ 
+}
